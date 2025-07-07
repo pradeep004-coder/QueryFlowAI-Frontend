8a80c77cd35c94f9bbbec439adbe54a102124e74
pradeep <pp0931275@gmail.com>
2025-07-07 17:10:09 +0530

    input section height change

--- src/components/InputSection.jsx
+++ src/components/InputSection.jsx
@@ -2,11 +2,11 @@ import Image from "next/image";
 
 function InputSection({askQuestion, textareaRef, query, handleInput, setQuery }) {
   return (
-    <div className='flex justify-center'>
+    <div className='flex justify-center sm:h-[80px]'>
         <form className="
                 w-[90%] lg:w-[70%]
                 bg-zinc-900
-                mb-5
+                mb-auto
                 p-1 pr-3
                 text-white
                 rounded-4xl
